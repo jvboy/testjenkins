774959a68252cd456ccbbbdf72685f4c5f5e386e
jvboy <1183588210@qq.com>
2019-05-26 17:11:36 +0800

    添加测试方法，不知道有用吗

--- src/test/java/com/example/testjenkins/testjenkins/TestjenkinsApplicationTests.java
+++ src/test/java/com/example/testjenkins/testjenkins/TestjenkinsApplicationTests.java
@@ -20,7 +20,7 @@ public class TestjenkinsApplicationTests {
 
 	@Test
 	public void testMain(){
-//		TestjenkinsApplication.main(null)
+		TestjenkinsApplication.main(new String[]{"123"});
 		try {
 			assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
 		} catch (AssertionError e) {
