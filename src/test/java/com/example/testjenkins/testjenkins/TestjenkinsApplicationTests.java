2201b1ed3e6a213b460c20026969c24184607757
jvboy <1183588210@qq.com>
2019-05-26 16:38:46 +0800

    添加测试方法，不知道有用吗

--- src/test/java/com/example/testjenkins/testjenkins/TestjenkinsApplicationTests.java
+++ src/test/java/com/example/testjenkins/testjenkins/TestjenkinsApplicationTests.java
@@ -5,12 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestjenkinsApplicationTests {
-
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	@Test
 	public void contextLoads() {
 	}
+
+	public void testMain(){
+		TestjenkinsApplication.main(null);
+		try {
+			assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+		} catch (AssertionError e) {
+			fail("\"message\" is not \"Hello World!\"");
+		}
+	}
+
 
 }
